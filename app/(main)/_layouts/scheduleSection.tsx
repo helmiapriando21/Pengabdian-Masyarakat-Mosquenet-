98a6b52115e38e01efbf15313a53d13ecb2c7cb6
Notevenmore <hafizsecond635@gmail.com>
2025-03-09 23:29:48 +0700

    Fix some bug in parameter api post routes

--- app/(main)/_layouts/scheduleSection.tsx
+++ app/(main)/_layouts/scheduleSection.tsx
@@ -3,7 +3,7 @@ import CalendarElement from "../_components/calendarElement";
 export default function ScheduleSection() {
     return (
         <div className="flex items-center justify-center w-screen h-screen">
-            <CalendarElement />
+            <CalendarElement masjid_id={null} />
         </div>
     );
     
