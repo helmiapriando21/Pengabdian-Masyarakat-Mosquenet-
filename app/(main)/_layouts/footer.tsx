e889dfdbd4f56b3767a1a5178459250e2e61e9ec
Notevenmore <hafizsecond635@gmail.com>
2025-03-10 04:34:45 +0700

    Give contact information

--- app/(main)/_layouts/footer.tsx
+++ app/(main)/_layouts/footer.tsx
@@ -2,12 +2,12 @@ export default function Footer() {
     return (
         <div className="flex flex-col items-center p-10 gap-3 bg-[#F9FAFB]">
             <p>Contact</p>
-            <p>Jalan Teuku Nyak Arief, Simprug</p>
+            <p>Jalan Teuku Nyak Arief, Simprug, Universitas Pertamina</p>
             <p>Kebayoran Lama, Jakarta Selatan, DKI Jakarta, 12220</p>
-            <p>Telp : +62 812-1149-9919 | (021) 50857540</p>
-            <p>Email : info@universitaspertamina.ac.id</p>
+            <p>Whatsapp : +6285162784180 </p>
+            <p>Email : amosquenet@gmail.com</p>
             <div className="bg-black h-[1px] w-screen"></div>
-            <p>Copyright by Pertamina University</p>
+            <p>Copyright by Mosquenet</p>
         </div>
     );
 }
